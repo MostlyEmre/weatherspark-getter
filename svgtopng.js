70d67da3011c980461c01484fb7da05e65125cb4
MostlyEmre <6238977+MostlyEmre@users.noreply.github.com>
2022-09-03 22:06:39 -0400

    Created svg-to-png script plus download button

--- svgtopng.js
+++ svgtopng.js
@@ -1,9 +1,30 @@
+const download = function (href, name) {
+  const link = document.createElement("a");
+  link.download = name;
+  link.innerText = "Download";
+  // link.style.opacity = "1";
+  document.querySelector(".download").append(link);
+  link.href = href;
+  // link.click();
+  // link.remove();
+};
+
 export default function () {
   const svg = document.querySelector("svg");
+  let clonedSVG = svg.cloneNode(true);
+  let outerHTML = clonedSVG.outerHTML;
+  let blob = new Blob([outerHTML], { type: "image/svg+xml;charset=utf-8" });
+  let blobURL = URL.createObjectURL(blob);
 
-  // convert svg to text
+  let image = new Image();
+  image.src = blobURL;
 
-  let s = new XMLSerializer();
-  let svgStr = s.serializeToString(svg);
-  console.log(svgStr);
+  let canvas = document.createElement("canvas");
+
+  image.onload = () => {
+    canvas.width = svg.clientWidth;
+    canvas.height = svg.clientHeight;
+    canvas.getContext("2d").drawImage(image, 0, 0, svg.clientWidth, svg.clientHeight);
+    download(canvas.toDataURL("image/png"), `${Date.now()}.png`);
+  };
 }
